b0af2f1351bf05131e45f4d79eb3c63ef609f078
Claire_Gao <gaoyinlu007@outlook.com>
2022-11-17 19:37:48 +0100

    hide stop button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strict";
- 
+
 //gamer name submit form
 let form = document.getElementById("gamerNameForm");
 form.onsubmit = function (event) {
   event.preventDefault();
-let name = form.elements.gamerName.value;
+  let name = form.elements.gamerName.value;
 
   // welcome message to the player
   document.getElementById("welcomeMsg").innerHTML =
@@ -22,14 +22,52 @@ function Start() {
   document.getElementById("letBegin").style.display = "none";
   // print "the game starts now"
   document.getElementById("startMessage").style.display = "block";
+  //print count down timer sign
+  document.getElementById("countDown").style.display = "block";
+  //document.getElementById("StopButton").style.display = "block";
+
+  clockStart();
 }
 
 //hide "The game starts now"
 document.getElementById("startMessage").style.display = "none";
+
 //Hide the start button
 document.getElementById("letBegin").style.display = "none";
 
 //copyright notice
-let date= new Date();
-let year= date.getFullYear();
-document.getElementById("currentYear").innerHTML=year;
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById("currentYear").innerHTML = year;
+
+//Hide the counting down timer function
+
+document.getElementById("countDown").style.display = "none";
+document.getElementById("StopButton").style.display = "none";
+
+//clock-start function
+function clockStart() {
+
+
+  const startingMinutes = 10;
+  let time = startingMinutes * 60; //all the seconds
+
+  const countdownEL = document.getElementById('countDown');
+  console.log('countDown');
+
+  setInterval(updateCountdown, 1000);
+
+  function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+
+
+    let seconds = time % 60;
+
+    //add 0 to number that less than 10 in front of them
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEL.innerHTML = `${minutes}:${seconds}`;
+
+    time--;
+  }
+}
